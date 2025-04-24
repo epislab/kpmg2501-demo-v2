bb7d865922d6bf97663332400df20b64161ee30c
parkjungkwan <pakjkwan@gmail.com>
2025-04-24 14:44:29 +0900

    .

---
+++ ai-server/titanic-service/app/domain/controller/titanic_controller.py
@@ -0,0 +1,24 @@
+from com.epislab.models.titanic.dataset import Dataset
+from com.epislab.models.titanic.titanic_service import TitanicService
+from icecream import ic
+
+
+class TitanicController:
+
+    service = TitanicService()
+
+    def preprocess(self, train, test):
+        pass
+    
+    def learning(self):
+        pass
+
+    def evaluation(self):
+        pass
+
+    def submit(self):
+        pass
+    
+ 
+    
+ 
